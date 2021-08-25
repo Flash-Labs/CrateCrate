2c2e4ee0e293e9823d3d7bf18cf8889dbbe64157
WillBAnders <WillBAnders@gmail.com>
2021-08-25 16:44:15 -0400

    Implement Crate methods

--- src/main/java/dev/flashlabs/cratecrate/component/Crate.java
+++ src/main/java/dev/flashlabs/cratecrate/component/Crate.java
@@ -4,7 +4,10 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
 import dev.flashlabs.cratecrate.CrateCrate;
 import dev.flashlabs.cratecrate.component.key.Key;
-import org.spongepowered.api.entity.living.player.Player;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.spongepowered.api.data.Keys;
+import org.spongepowered.api.entity.living.player.server.ServerPlayer;
+import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.item.inventory.ItemStack;
 import org.spongepowered.api.item.inventory.ItemStackSnapshot;
 import org.spongepowered.api.util.Tuple;
@@ -15,11 +18,14 @@ import org.spongepowered.configurate.serialize.SerializationException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
 
 public final class Crate extends Component<Void> {
 
     public static final CrateType TYPE = new CrateType();
     public static final Map<String, Type<? extends Crate, ?>> TYPES = Maps.newHashMap();
+
+    private static final Random RANDOM = new Random();
 
     private final Optional<String> name;
     private final Optional<ImmutableList<String>> lore;
@@ -43,19 +49,43 @@ public final class Crate extends Component<Void> {
         this.rewards = rewards;
     }
 
+    /**
+     * Returns the name of this crate, defaulting to the id.
+     */
     @Override
-    public net.kyori.adventure.text.Component getName(Optional<Void> value) {
-        throw new UnsupportedOperationException(); //TODO
+    public net.kyori.adventure.text.Component getName(Optional<Void> ignored) {
+        return name.map(s -> LegacyComponentSerializer.legacyAmpersand().deserialize(s))
+            .orElseGet(() -> net.kyori.adventure.text.Component.text(id));
     }
 
+    /**
+     * Returns the lore of this crate, defaulting to an empty list.
+     */
     @Override
-    public List<net.kyori.adventure.text.Component> getLore(Optional<Void> value) {
-        throw new UnsupportedOperationException(); //TODO
+    public List<net.kyori.adventure.text.Component> getLore(Optional<Void> ignored) {
+        return lore
+            .map(l -> l.stream()
+                .map(s -> LegacyComponentSerializer.legacyAmpersand().deserialize(s).asComponent())
+                .toList())
+            .orElseGet(List::of);
     }
 
+    /**
+     * Returns the icon of this crate, defaulting to a chest. If the icon does
+     * not have a defined display name or lore, it is set to this crate's
+     * name/lore.
+     */
     @Override
-    public ItemStack getIcon(Optional<Void> value) {
-        throw new UnsupportedOperationException(); //TODO
+    public ItemStack getIcon(Optional<Void> ignored) {
+        var base = icon.map(ItemStackSnapshot::createStack)
+            .orElseGet(() -> ItemStack.of(ItemTypes.CHEST, 1));
+        if (base.get(Keys.DISPLAY_NAME).isEmpty()) {
+            base.offer(Keys.DISPLAY_NAME, getName(Optional.empty()));
+        }
+        if (base.get(Keys.LORE).isEmpty()) {
+            base.offer(Keys.LORE, getLore(Optional.empty()));
+        }
+        return base;
     }
 
     public ImmutableList<Tuple<? extends Key, Integer>> getKeys() {
@@ -66,12 +96,29 @@ public final class Crate extends Component<Void> {
         return rewards;
     }
 
-    public void open(Player player, Location<?, ?> location) {
-        throw new UnsupportedOperationException(); //TODO
+    public void open(ServerPlayer player, Location<?, ?> location) {
+        give(player, location, roll(player));
     }
 
-    public void give(Player player, Location<?, ?> location, Tuple<? extends Reward, Integer> reward) {
-        throw new UnsupportedOperationException(); //TODO
+    public void give(ServerPlayer player, Location<?, ?> location, Tuple<? extends Reward, Integer> reward) {
+        reward.first().give(player.user());
+    }
+
+    /**
+     * Returns a random reward rolled from this crate. Currently, rewards are
+     * not dependent on the player but this is likely to change in the future.
+     */
+    public Tuple<? extends Reward, Integer> roll(ServerPlayer player) {
+        int sum = rewards.stream().mapToInt(Tuple::second).sum();
+        int selection = RANDOM.nextInt(sum);
+        for (Tuple<? extends Reward, Integer> reward : rewards) {
+            selection -= reward.second();
+            if (selection < 0) {
+                return reward;
+            }
+        }
+        //TODO: Handle properly for player dependent rewards
+        throw new AssertionError("No available rewards.");
     }
 
     public static final class CrateType extends Type<Crate, Void> {
